3643e555ac6fc3c10158cd61d0619e2fe88b7072
Adrian Vollmer <adrian.vollmer@syss.de>
2019-04-02 11:25:33 +0200

    enable preparation via ssh

--- prepare-image.sh
+++ prepare-image.sh
@@ -4,44 +4,57 @@
 # i.e. it installs a systemd service and copies the app as a tarball
 
 set -e
-set -u
 
 IMG="$1"
+PORT="$2"
+
+set -u
+
 TARBALL=/tmp/lg.tar.gz
 BOOTSTRAP_DIR="$(dirname "$0")/bootstrap"
 
 if [ -z "$IMG" ]; then
     cat << EOF
-This script prepares a Raspbian image for the Lauschgerät.
+This script prepares a Raspbian image or a remote machine via SSH for the Lauschgerät.
 
 Usage:
-$0 <PATH>
+$0 <PATH>|<SSHSERVER> [<PORT>]
 EOF
     exit 1
 fi
-
-echo "Checking prerequisites..."
-
-for com in guestfish ; do
-    command -v "$com" >/dev/null 2>&1 || {
-        echo >&2 "$com required, but it's not installed. Aborting."
-        exit 1
-    }
-done
-
-echo "All good, preparing the image..."
 
 tar czf "$TARBALL" -C "$(dirname "$0")" --exclude=testing \
     "lg-server" \
     "lauschgeraet" \
     "lauschgerät.py"
 
-guestfish -a "$IMG" -m /dev/sda2 <<_EOF_
+if [ -f "$IMG" ] ; then
+    echo "Checking prerequisites..."
+
+    for com in guestfish ; do
+        command -v "$com" >/dev/null 2>&1 || {
+            echo >&2 "$com required, but it's not installed. Aborting."
+            exit 1
+        }
+    done
+
+    echo "All good, preparing the image..."
+
+
+    guestfish -a "$IMG" -m /dev/sda2 <<_EOF_
 copy-in $BOOTSTRAP_DIR/lauschgeraetd /usr/sbin
 copy-in $TARBALL /root/
 copy-in $BOOTSTRAP_DIR/lauschgeraet.service /lib/systemd/system/
 ln-s /lib/systemd/system/lauschgeraet.service /etc/systemd/system/multi-user.target.wants/lauschgeraet.service
 _EOF_
+else
+    [ -z "$PORT" ] && PORT=22
+    scp -P "$PORT" $TARBALL $BOOTSTRAP_DIR/lauschgeraet.service $BOOTSTRAP_DIR/lauschgeraetd "$IMG:/root/"
+    ssh -p "$PORT" $IMG "ln -s /lib/systemd/system/lauschgeraet.service \
+    /etc/systemd/system/multi-user.target.wants/lauschgeraet.service; \
+    cp /root/lauschgeraetd /usr/sbin; \
+    cp /root/lauschgeraet.service /lib/systemd/system/"
+fi
 
 rm -f "$TARBALL"
 
